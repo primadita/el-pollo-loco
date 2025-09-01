1b4f3e235e9b648ab755b5beac582891374cc5a4
primadita <pulomas10@hotmail.com>
2025-09-01 23:34:37 +0200

    doc: add JSDOC to layer background

--- models/desert-first-layer.class.js
+++ models/desert-first-layer.class.js
@@ -1,17 +1,28 @@
 import { ImageManager } from "../js/image-manager.class.js";
 import { Background } from "./background.class.js";
 
+/**
+ * Represents the first layer of the desert background.
+ * @extends Background
+ */
 export class DesertFirstLayer extends Background{
     // #region ATTRIBUTES
     static XPOS = -2 * canvas.width;
     // #endregion
 
+    /**
+     * Creates a new DesertFirstLayer.
+     */
     constructor(){
         super({_x: DesertFirstLayer.XPOS, _img: ImageManager.BACKGROUND.fullDesert[2]});
         DesertFirstLayer.generateNewXPos(this.width);
     }
 
     // #region METHODS
+    /**
+     * Generates a new X position for the layer.
+     * @param {number} val - Value to add to XPOS.
+     */
     static generateNewXPos(val){
         DesertFirstLayer.XPOS += val;
     }
--- models/desert-third-layer.class.js
+++ models/desert-third-layer.class.js
@@ -1,17 +1,28 @@
 import { ImageManager } from "../js/image-manager.class.js";
 import { Background } from "./background.class.js";
 
+/**
+ * Represents the third layer of the desert background.
+ * @extends Background
+ */
 export class DesertThirdLayer extends Background{
     // #region ATTRIBUTES
     static XPOS = -2 * canvas.width;
     // #endregion
 
+    /**
+     * Creates a new DesertSecondLayer.
+     */
     constructor(){
         super({_x: DesertThirdLayer.XPOS, _img: ImageManager.BACKGROUND.fullDesert[0]});
         DesertThirdLayer.generateNewXPos(this.width);
     }
 
     // #region METHODS
+    /**
+     * Generates a new X position for the layer.
+     * @param {number} val - Value to add to XPOS.
+     */
     static generateNewXPos(val){
         DesertThirdLayer.XPOS += val;
     }
